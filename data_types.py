f35697a5177eb8b8958245867b41f02eed454f39
Hashim-ali1 <ha2005947@gmail.com>
2026-07-21 12:44:50 +0530

    Add data types examples in data_types.py

---
+++ data_types.py
@@ -0,0 +1,39 @@
+# Integer 
+a = 3
+b = 4
+print(a)
+print(b)
+
+# Float 
+a = 1.2
+b = 5.4
+print(c)
+print(d)
+
+# Complex 
+a = 2+6j
+print(a)
+
+# String 
+a = "This is a String DataType"
+print(a)
+
+# List 
+a = ["Math","Bio","AI","ML","Biology"]
+print(a)
+
+# Tuples 
+a = (1, 2 , 3 , 4 , 5 )
+print(a)
+
+# Boolean 
+a = True 
+b = False
+
+# Set 
+a = {"A","B","C","D"}
+print(a)
+
+# Dictionary
+a = {"name":"Hash", "age":40,"phone_no":33202020}
+print(a)
